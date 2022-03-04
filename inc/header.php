1e38e2f5f84492a039339c444a9436a8d9c26076
YanisLandrieu <91880045+YanisLandrieu@users.noreply.github.com>
2022-03-04 09:49:19 +0100

    composer

--- inc/header.php
+++ inc/header.php
@@ -1,10 +1,42 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Allociné</title>
+    <!-- css -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <!-- js -->
+    <script src="../assets/js/script.js"></script>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
+
 <body>
-    
+<!-- Header -->
+    <nav class="navbar navbar-expand-lg fixed-top navbar-scroll shadow-0" style="background-color: #fecc00;">
+        <div class="container">
+            <a class="navbar-brand" href="#">Allociné</a>
+            <button class="navbar-toggler ps-0" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarAllociné" aria-controls="navbarAllociné" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="d-flex justify-content-start align-items-center">
+                    <i class="fas fa-bars"></i>
+                </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarExample01">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link px-3" href="#!">Prochaines Sorties</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="#!">Top Films</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="#!">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<!-- Header -->
