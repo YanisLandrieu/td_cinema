554c6b9af8554a59401f49d70b17c2d05db36c32
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-03-07 12:15:21 +0100

    description

--- display.php
+++ display.php
@@ -5,12 +5,13 @@
     foreach (get_films()->results as $key => $value) : ?>
         <?php $title = get_films()->results[$key]->original_title; ?>
         <?php $posterPath = get_films()->results[$key]->poster_path; ?>
+        <?php $desc = get_films()->results[$key]->overview; ?>
         <div class='col-12 col-sm-6 col-xl-4 mb-3'>
             <div class='row no-gutters'>
                 <div class='col-md-7 pl-2'>
                     <h3 class="pt-3 pt-md-0"><?= $title; ?></h5>
                     <img class='img-fluid' src='https://image.tmdb.org/t/p/w500<?= $posterPath; ?>' />
-                    <p class='text'>Description du film</p>
+                    <p class='text'><?= $desc; ?></p>
                     <button type="button" class="btn btn-primary" style="background-color: #fecc00; border-color:black; color:black;">En savoir plus</button>
                 </div>
             </div>
