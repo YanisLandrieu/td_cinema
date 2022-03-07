65b19b7ea49e2ea09e2503c896e1eeceb6cc1638
YanisLandrieu <91880045+YanisLandrieu@users.noreply.github.com>
2022-03-07 13:14:57 +0100

    paginations

--- details.php
+++ details.php
@@ -1,12 +1,14 @@
 <?php include_once('./inc/header.php'); ?>
-
+<?php include_once('./functions.php'); ?>
 
 <div class="container dotted p-4 mt-4">
     <div class="row">
         <div class="col-12">
             <h2 class="text-center mt-3">Films à voir</h2>
             <div id="filmTable" class="row mt-3 p-3">
-                
+                <div class='col-md-7 pl-2'>
+                   
+                </div>
             </div>
         </div>
     </div>
