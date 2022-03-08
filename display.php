243be195c4b4a26dcbc9fd7e2aa5aefebf5f5c49
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-03-08 10:25:13 +0100

    com display

--- display.php
+++ display.php
@@ -1,21 +1,21 @@
 <?php include_once('./functions.php'); ?>
 
-<?php     
-    $filmsDisplay = '';
-    // On boucle sur le résultat de get_films()
-    foreach (get_films($_GET['page'])->results as $key => $value) : ?>
-        <?php $title = get_films($_GET['page'])->results[$key]->original_title; ?>
-        <?php $posterPath = get_films($_GET['page'])->results[$key]->poster_path; ?>
-        <?php $desc = get_films($_GET['page'])->results[$key]->overview; ?>
-        <?php $id = get_films($_GET['page'])->results[$key]->id; ?>
-        <div class='col-12 col-sm-6 col-xl-4 mb-3'>
-            <div class='row no-gutters'>
-                <div class='col-md-7 pl-2'>
-                    <h3 class="pt-3 pt-md-0"><?= $title; ?></h5>
-                    <img class='img-fluid' src='https://image.tmdb.org/t/p/original<?= $posterPath; ?>' />
-                    <p class='text'><?= $desc; ?></p>
-                    <a href='http://td_cinema.test/details.php?id=<?= $id; ?>'><button type="button" class="btn btn-primary" style="background-color: #fecc00; border-color:black; color:black;">En savoir plus</button></a>
-                </div>
+<!-- On boucle sur le résultat de get_films() -->
+<?php foreach (get_films($_GET['page'])->results as $key => $value) : ?>
+    <?php $title = get_films($_GET['page'])->results[$key]->original_title; ?>
+    <?php $posterPath = get_films($_GET['page'])->results[$key]->poster_path; ?>
+    <?php $desc = get_films($_GET['page'])->results[$key]->overview; ?>
+    <?php $id = get_films($_GET['page'])->results[$key]->id; ?>
+    <!-- Début partie html -->
+    <div class='col-12 col-sm-6 col-xl-4 mb-3'>
+        <div class='row no-gutters'>
+            <div class='col-md-7 pl-2'>
+                <h3 class="pt-3 pt-md-0"><?= $title; ?></h5>
+                <img class='img-fluid' src='https://image.tmdb.org/t/p/original<?= $posterPath; ?>' />
+                <p class='text'><?= $desc; ?></p>
+                <a href='http://td_cinema.test/details.php?id=<?= $id; ?>'><button type="button" class="btn btn-primary" style="background-color: #fecc00; border-color:black; color:black;">En savoir plus</button></a>
             </div>
         </div>
-    <?php endforeach; ?>
+    </div>
+    <!-- Fin partie html -->
+<?php endforeach; ?>
