2cb1c3a043530dd0374ff607d48385538a9309c9
YanisLandrieu <91880045+YanisLandrieu@users.noreply.github.com>
2022-03-08 10:22:19 +0100

    Details front

--- details.php
+++ details.php
@@ -1,13 +1,19 @@
 <?php include_once('./inc/header.php'); ?>
 <?php include_once('./functions.php'); ?>
 
-<div class="container dotted p-4 mt-4">
+<div class="containers">
     <div class="row">
-        <div class="col-12">
-            <h2 class="text-center mt-3">Films à voir</h2>
-            <div id="filmTable" class="row mt-3 p-3">
-                <div class='col-md-7 pl-2'>
-                   
+        <h3></h3>
+        <div id="filmTable" class="" style="margin-top: 50px;">
+            <div style="display: flex; flex-direction:row; flex-wrap:nowrap; justify-content:center" >
+                <div class="item" style="margin-top: 20px; margin-bottom:20px"><img src="./assets/img/spider-man-no-way-home-affiche-2.jpg" style="height: 500px; width:400px" alt=""></div>
+                <div class="item" style="margin-left: 10px; padding: 10px;">
+                    <h1>Titre</h1>
+                    <h5>Date de Sortie</h5>
+                    <h3>Synopsis</h3>
+                    <p>Synonpsis du film</p>
+                    <h3>Budget</h3>
+                    <p>budget du film</p>
                 </div>
             </div>
         </div>
