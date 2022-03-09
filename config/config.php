dc2051f3e6aa6df83ab71a90f17c40d396e7613b
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-03-09 11:44:28 +0100

    linkdisplay

--- config/config.php
+++ config/config.php
@@ -5,4 +5,5 @@ const base_url = "https://api.themoviedb.org/3";
 // On initialise notre clé
 const authentification_parameters = "api_key=ee7a7c67e85b3b17c01fe7b891ebdb11";
 
+var_dump($_SERVER['SERVER_NAME']);
 ?>
